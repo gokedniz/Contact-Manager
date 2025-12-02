162077c2a5e6287b1e59e2834b653448b8c4b48c
gokdeniz <gokdenizdemircioglu@gmail.com>
2025-12-02 20:27:32 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import ui.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-        // Update passwords to hash (One-time setup for provided SQL data)
-        // com.cmpe343.project.util.DatabaseSetup.updatePasswordsToHash();
-
-        // Start the Console UI
         new ConsoleUI().start();
     }
 }
